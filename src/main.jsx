1eb8049c02e31f8045a3e381e3fc4b2061989803
Win Min <winminthuzar1@gmail.com>
2026-08-02 09:28:19 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import PasswordGate from "./components/PasswordGate";
+import "./styles.css";
+import "./password-gate.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PasswordGate>
+      <App />
+    </PasswordGate>
+  </React.StrictMode>
+);
